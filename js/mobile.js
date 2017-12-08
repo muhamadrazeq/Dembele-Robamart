3a89568d1e74b27d9b4e12637c062b3d6d58e69e
muhamadrazeq <muhamad.razeq@students.jmcss.org>
2017-12-08 13:09:57 -0600

    Add files via upload

---
+++ js/mobile.js
@@ -0,0 +1,85 @@
+window.onload = function(){
+	var getNavi = document.getElementById('navigation');
+
+	var mobile = document.createElement("span");
+	mobile.setAttribute("id","mobile-navigation");
+	getNavi.parentNode.insertBefore(mobile,getNavi);
+
+	document.getElementById('mobile-navigation').onclick = function(){
+		var a = getNavi.getAttribute('style');
+		if(a){
+			getNavi.removeAttribute('style');
+			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile-menu.png)';
+		} else {
+			getNavi.style.display='block';
+			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile-close.png)';
+		}
+	};
+	var getElm = getNavi.getElementsByTagName("LI");
+	for(var i=0;i<getElm.length;i++){
+		if(getElm[i].children.length>1){
+			var smenu = document.createElement("span");
+			smenu.setAttribute("class","mobile-submenu");
+			smenu.setAttribute("OnClick","submenu("+i+")");
+			getElm[i].appendChild(smenu);
+		};
+	};
+	submenu = function (i){
+		var sub = getElm[i].children[1];
+		var b = sub.getAttribute('style');
+		if(b){
+			sub.removeAttribute('style');
+			getElm[i].lastChild.style.backgroundImage='url(images/mobile-expand.png)';
+			getElm[i].lastChild.style.backgroundColor='rgba(11, 163, 156, 0.7)';
+		} else {
+			sub.style.display='block';
+			getElm[i].lastChild.style.backgroundImage='url(images/mobile-collapse.png)';
+			getElm[i].lastChild.style.backgroundColor='rgba(0,0,0,0.8)';
+		}
+	};
+};
+
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
